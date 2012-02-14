d4b8dfcccf7a1fa0ec4a72136efabcdb676c9584
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2012-02-14 14:32:34 +0000

    Imported Upstream version 20100827
    Downloaded manually via:
     wget --mirror --no-parent http://www.netlib.org/f2c
    followed by manual deletion of extraneous files.

--- src/version.c
+++ src/version.c
@@ -1,2 +1,2 @@
-char F2C_version[] = "20090411";
-char xxxvers[] = "\n@(#) FORTRAN 77 to C Translator, VERSION 20090411\n";
+char F2C_version[] = "20100827";
+char xxxvers[] = "\n@(#) FORTRAN 77 to C Translator, VERSION 20100827\n";
