85c8d8de771e3ac06127c51211355fd0b52e7a12
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2012-02-14 12:44:44 +0000

    Imported Upstream version 20090411

--- src/version.c
+++ src/version.c
@@ -1,2 +1,2 @@
-char F2C_version[] = "20061008";
-char xxxvers[] = "\n@(#) FORTRAN 77 to C Translator, VERSION 20061008\n";
+char F2C_version[] = "20090411";
+char xxxvers[] = "\n@(#) FORTRAN 77 to C Translator, VERSION 20090411\n";
